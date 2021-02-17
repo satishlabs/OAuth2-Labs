a33364e02562b80f5fb05dfea6945030d683f133
Satish Prasad <spd61185@gmail.com>
2021-02-17 14:48:56 +0530

    ro-password-type-server

---
+++ ro-password-type-server/src/main/java/com/oauth/ResourceOwnerPasswordTypeServerApplication.java
@@ -0,0 +1,13 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceOwnerPasswordTypeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceOwnerPasswordTypeServerApplication.class, args);
+	}
+
+}

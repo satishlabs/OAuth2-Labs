191bf07b6285895709e8e1413e2c9818985511d5
Satish Prasad <spd61185@gmail.com>
2021-02-17 19:54:21 +0530

    Client Credentials Grant Type

---
+++ client-credentials-type-server/src/main/java/com/oauth/ClientCredentialTypeServerApplication.java
@@ -0,0 +1,12 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCredentialTypeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCredentialTypeServerApplication.class, args);
+	}
+}
